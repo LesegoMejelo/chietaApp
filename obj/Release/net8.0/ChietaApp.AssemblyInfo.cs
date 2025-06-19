7b1db4bf85cffec8e0e03ed8ac4b84923220a62b
Lilitha Kunene <KNNLIL002@myuct.ac.za>
2025-06-19 17:09:36 +0200

    change

--- obj/Release/net8.0/ChietaApp.AssemblyInfo.cs
+++ obj/Release/net8.0/ChietaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579b6d80ca965f46728f287dded52a4c3dc14bd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81dfc243e991fba27b141246c145c13d35d2fb85")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
